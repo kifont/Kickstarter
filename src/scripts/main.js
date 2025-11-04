ee31180ee3e16e7723e3abf143ff67a098d26e0f
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-11-04 18:40:14 +0200

    finally fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,26 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.top-bar__burger');
   const closeBtn = document.querySelector('.menu__close');
   const navLinks = document.querySelector('.menu__nav .nav__link');
-  const body = document.body;
-
-  let scrollPosition = 0;
 
   const openMenu = () => {
-    scrollPosition = window.scrollY;
     menu.classList.add('active');
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollPosition}px`;
-    body.style.left = '0';
-    body.style.right = '0';
+    document.body.classList.add('no-scroll');
   };
 
   const closeMenu = () => {
     menu.classList.remove('active');
-    body.style.position = '';
-    body.style.top = '';
-    body.style.left = '';
-    body.style.right = '';
-    window.scrollTo(0, scrollPosition);
+    document.body.classList.remove('no-scroll');
   };
 
   burger.addEventListener('click', (e) => {
